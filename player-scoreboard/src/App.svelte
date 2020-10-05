d3b386163172662b7a3e1bf4e64974c4e16d6733
Mateusz Kopij <sagasu@gmail.com>
2020-10-05 20:16:11 +0100

    applying styles

--- player-scoreboard/src/App.svelte
+++ player-scoreboard/src/App.svelte
@@ -4,28 +4,19 @@
 </script>
 
 <main>
-	<h1>{name}</h1>
-	<h1>{points}</h1>
+	<div class="container">
+		<div class="card">
+			<h1>{name}</h1>
+			<h3>{points}</h3>
+		</div>
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
