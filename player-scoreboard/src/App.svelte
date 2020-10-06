a5fe69a5bfe0dab03cc18568ef3dfa17d6c42827
Mateusz Kopij <sagasu@gmail.com>
2020-10-06 21:08:47 +0100

    adding button to control menu

--- player-scoreboard/src/App.svelte
+++ player-scoreboard/src/App.svelte
@@ -1,20 +1,28 @@
 <script>
 	let name = "Matt";
 	let points = 100;
+	let showControls = true;
 
 	const addPoint = () => points += 1;
 	const removePoint = () => points -= 1;
+	const toggleControls = () => (showControls = !showControls);
 
 </script>
 
 <main>
 	<div class="container">
 		<div class="card">
-			<h1>{name}</h1>
+			<h1>{name}
+				<button class="btn btn-sm" on:click={toggleControls}>
+					{#if showControls}-{:else}+{/if}
+				</button>
+			</h1>
 			<h3>{points}</h3>
-			<button class="btn" on:click={addPoint}>+1</button>
-			<button class="btn" on:click={removePoint}>-1</button>
-			<input type="number" bind:value={points}>
+			{#if showControls}
+				<button class="btn" on:click={addPoint}>+1</button>
+				<button class="btn" on:click={removePoint}>-1</button>
+				<input type="number" bind:value={points}>
+			{/if}
 		</div>
 	</div>
 </main>
@@ -25,5 +33,8 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+	h3 {
+		margin: bottom;
 	}
 </style>
