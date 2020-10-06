d5526f3b53f7017809c4c4ec9f454eb2a2059299
Mateusz Kopij <sagasu@gmail.com>
2020-10-06 21:58:48 +0100

    introducing players

--- player-scoreboard/src/App.svelte
+++ player-scoreboard/src/App.svelte
@@ -1,43 +1,30 @@
 <script>
 	import Navbar from './Navbar.svelte';
+	import Player from './Player.svelte';
 
-	let name = "Matt";
-	let points = 100;
-	let showControls = true;
-
-	const addPoint = () => points += 1;
-	const removePoint = () => points -= 1;
-	const toggleControls = () => (showControls = !showControls);
-
+	let players = [{
+		name: "Matt",
+		points: 3
+	},
+	{	name: "Aneta 'The Unicorn Queen'",
+		points:12}
+		,
+	{
+		name: "Noti",
+		points: 5
+	}];
 </script>
 
 <main>
 	<Navbar></Navbar>
 	<div class="container">
-		<div class="card">
-			<h1>{name}
-				<button class="btn btn-sm" on:click={toggleControls}>
-					{#if showControls}-{:else}+{/if}
-				</button>
-			</h1>
-			<h3>{points}</h3>
-			{#if showControls}
-				<button class="btn" on:click={addPoint}>+1</button>
-				<button class="btn" on:click={removePoint}>-1</button>
-				<input type="number" bind:value={points}>
-			{/if}
-		</div>
+		{#if players.length === 0}
+			<p>No Players</p>
+		{:else}
+			{#each players as player}
+			<Player name={player.name} points={player.points}/>
+			{/each}
+		{/if}
 	</div>
 </main>
 
-<style>
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	h3 {
-		margin: bottom;
-	}
-</style>
---
+++ player-scoreboard/src/Player.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let name;
+    export let points;
+    let showControls = true;
+
+    const addPoint = () => (points += 1);
+    const removePoint = () => (points -= 1);
+    const toggleControls = () => (showControls = !showControls);
+</script>
+
+<style>
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+    h3 {
+        margin: bottom;
+    }
+</style>
+
+<div class="card">
+    <h1>
+        {name}
+        <button class="btn btn-sm" on:click={toggleControls}>
+            {#if showControls}-{:else}+{/if}
+        </button>
+    </h1>
+    <h3>{points}</h3>
+    {#if showControls}
+        <button class="btn" on:click={addPoint}>+1</button>
+        <button class="btn" on:click={removePoint}>-1</button>
+        <input type="number" bind:value={points} />
+    {/if}
+</div>
