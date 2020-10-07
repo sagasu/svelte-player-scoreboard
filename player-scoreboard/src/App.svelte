ff08fd1f1bb87c39ab71c8b836b4eec9464a2f7a
Mateusz Kopij <sagasu@gmail.com>
2020-10-07 17:54:49 +0100

    adding add player functionality

--- player-scoreboard/src/App.svelte
+++ player-scoreboard/src/App.svelte
@@ -1,23 +1,31 @@
 <script>
 	import Navbar from './Navbar.svelte';
 	import Player from './Player.svelte';
+	import AddPlayer from './AddPlayer.svelte';
 
 	let players = [{
 		name: "Matt",
 		points: 3
 	},
-	{	name: "Aneta 'The Unicorn Queen'",
+	//the unicor queen
+	{	name: "🦄Aneta👑",
 		points:12}
 		,
 	{
-		name: "Noti",
+		name: "🐕Noti🐶",
 		points: 5
 	}];
+
+	const addPlayer = (e) => {
+		const newPlayer = e.detail;
+		players = [...players, newPlayer];
+	};
 </script>
 
 <main>
 	<Navbar></Navbar>
 	<div class="container">
+		<AddPlayer on:addplayer={addPlayer}/>
 		{#if players.length === 0}
 			<p>No Players</p>
 		{:else}
